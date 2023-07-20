bcc0c5397671b6f7c5d1c2c92124f96dd3fbb877
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-07-20 14:28:31 -0500

    Created UI, made Js to create the drawing grid

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.getElementById('grid');
+
+function createGrid(gridSize){
+    grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize},1fr)`
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        grid.appendChild(gridElement);
+    }
+}
